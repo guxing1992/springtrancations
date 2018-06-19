51e07c0a0d1140c8e9d74d54853b5d4015f42980
Zhang <Zhang@Zhang-PC>
2018-06-19 18:23:14 +0800

    添加事务

--- src/main/java/com/test/springtrancations/service/UserServiceImpl.java
+++ src/main/java/com/test/springtrancations/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.test.springtrancations.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.test.springtrancations.db1.dao.UserDb1Mapper;
 import com.test.springtrancations.db2.dao.UserDb2Mapper;
@@ -14,13 +15,15 @@ public class UserServiceImpl implements UserService {
 	UserDb2Mapper userDb2Mapper;
 	/**
 	 * 需要对2个数据库进行操作
+	 * @throws Exception 
 	 */
-	@Override
-	public int addUser(UserVO userVO) throws Exception {
+	@Transactional(rollbackFor=Exception.class,noRollbackFor=RuntimeException.class)
+	public int addUser(UserVO userVO) throws Exception{
 		// TODO Auto-generated method stub
 		userDb1Mapper.addUser(userVO);
 		userDb2Mapper.addUser(userVO);
-		return 1;
+//		return 1;
+		throw new RuntimeException("自定义异常");
 	}
 
 }
--- src/test/java/com/test/springtrancations/test/MyTest.java
+++ src/test/java/com/test/springtrancations/test/MyTest.java
@@ -1,6 +1,5 @@
 package com.test.springtrancations.test;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,25 +10,24 @@ import com.test.springtrancations.service.UserService;
 import com.test.springtrancations.vo.UserVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath*:context/datasource-context.xml")
+@ContextConfiguration(locations = "classpath*:context/datasource-context.xml")
 public class MyTest {
 	@Autowired
 	UserService userService;
+
 	@Test
-	public void test1() {
-		for (int i = 0; i < 1000; i++) {
-			UserVO userVO = new UserVO();
-			userVO.setUsername("liuliu11"+i);
-			userVO.setPassword("00800821"+i);
-			userVO.setSex(3+i);
-			try {
-				userService.addUser(userVO);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	public void test1(){
+		UserVO userVO = new UserVO();
+		userVO.setUsername("wan123");
+		userVO.setPassword("123123");
+		userVO.setSex(1);
+
+		try {
+			userService.addUser(userVO);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		
+
 	}
 }
